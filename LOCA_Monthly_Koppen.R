e3f45a549928e67ecfff724d9867e5d479317fa7
William J. Capehart <William.Capehart@sdsmt.edu>
2019-02-09 16:48:48 -0700

    Update LOCA_Monthly_Koppen.R

--- LOCA_Monthly_Koppen.R
+++ LOCA_Monthly_Koppen.R
@@ -821,7 +821,7 @@ ensembles = c( "ACCESS1-0_r1i1p1",
         # Create Parallel Block Here
         
         print(str_c("     "))
-        print(str_c("   - Beginning Latitude Loop for ", Ensemble ," ", (lon_i*100.0/length(longitue)),"%" ))
+        print(str_c("   - Beginning Latitude Loop for ", Ensemble ," ", (lon_i*100.0/length(longitude)),"%" ))
         print(str_c("     "))
         
         for (Latitude in latitude) 
